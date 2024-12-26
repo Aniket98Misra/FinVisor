7b83fd7c630b702e00526c8e9efb1a8659e69625
Aniket98Misra <misra98aniket@gmail.com>
2024-12-26 13:19:34 +0530

    Second row fully functional

--- server/index.js
+++ server/index.js
@@ -7,7 +7,9 @@ import helmet from "helmet";
 import morgan from "morgan";
 import kpiRoutes from "./routes/kpi.js";
 import KPI from "./models/KPI.js";
-import { kpis } from "./data/data.js";
+import { kpis, products } from "./data/data.js";
+import productRoutes from "./routes/product.js";
+import Product from "./models/Product.js";
 
 /*CONFIGURATION */
 dotenv.config();
@@ -22,6 +24,7 @@ app.use(cors());
 
 /*Routes */
 app.use("/kpi", kpiRoutes);
+app.use("/product", productRoutes);
 /*Mongoose Setup */
 const PORT = process.env.PORT || 9000;
 mongoose
@@ -34,5 +37,6 @@ mongoose
     /*Add data one time only*/
     // await mongoose.connection.db.dropDatabase();
     // KPI.insertMany(kpis);
+    Product.insertMany(products);
   })
   .catch((error) => console.log(`${error} did not connect`));
